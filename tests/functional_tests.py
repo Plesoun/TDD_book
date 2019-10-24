291ea0609c04f6978e1b8cfdc1c0e4e9040f91ab
Jakub Schier <jakub.schier@mojemana.cz>
2019-10-24 12:39:59 +0200

    Initial commit

---
+++ tests/functional_tests.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox(executable_path='C:\\Users\\jakub\\OneDrive\\Documents\\Python Scripts\\TDD\\geckodriver.exe')
+browser.get("http://localhost:8000")
+
+assert "Django" in browser.title
+
+browser.close()
